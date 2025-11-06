1a04064a93e47f43b762bb1f6af60a214c0ffeea
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-11-06 15:52:32 +0530

    refactor(Home): remove PricingSection component to streamline homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,7 +8,6 @@ import OurServices from '@/components/homepage/our-services';
 import HeroSection from '@/components/homepage/hero-section';
 import AboutSection from '@/components/homepage/about-section';
 import FAQSection from '@/components/homepage/faq-section';
-import PricingSection from '@/components/homepage/pricing-section';
 import ContactSection from '@/components/homepage/contact-section';
 import SubscriptionsSection from '@/components/core/Subscriptions';
 
